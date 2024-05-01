e034f743aac9e3504f37fb3943fa5127a32477e0
Yash Raut <yashraut25122002@gmail.com>
2024-05-01 22:45:33 +0530

    recruiter files

---
+++ models/recruiter.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const recruiterSchema = new mongoose.Schema({
+    companyName: {
+        type: String,
+        required: true
+    },
+    companyWebsite: {
+        type: String,
+        required: true
+    },
+    companyDescription: {
+        type: String,
+        required: true
+    },
+    recruiterName: {
+        type: String,
+        required: true
+    },
+    recruiterTitle: {
+        type: String,
+        required: true
+    },
+    recruiterEmail: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    recruiterPhoneNumber: {
+        type: String,
+        required: true
+    },
+    companyPhoto: {
+        type: String,
+    },
+    Rid: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Recruiter = mongoose.model('Recruiter', recruiterSchema);
+
+module.exports = Recruiter;
---
+++ public/helper/questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "A train running at the speed of 60 km/hr crosses a pole in 9 seconds. What is the length of the train?",
+        answer: "150 metres",
+        options: [
+            "120 metres",
+            "180 metres",
+            "324 metres",
+            "150 metres"
+        ]
+    },
+    {
+        numb: 2,
+        question: "A person crosses a 600 m long street in 5 minutes. What is his speed in km per hour?",
+        answer: "7.2",
+        options: [
+            "3.6",
+            "7.2",
+            "8.4",
+            "10"
+        ]
+    },
+    {
+        numb: 3,
+        question: "It was Sunday on Jan 1, 2006. What was the day of the week Jan 1, 2010?",
+        answer: "Friday",
+        options: [
+            "Sunday",
+            "Saturday",
+            "Friday",
+            "Wednesday"
+        ]
+    },
+    {
+        numb: 4,
+        question: "In the first 10 overs of a cricket game, the run rate was only 3.2. What should be the run rate in the remaining 40 overs to reach the target of 282 runs?",
+        answer: "6.25",
+        options: [
+            "6.25",
+            "6.5",
+            "6.75",
+            "7"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A and B together have Rs. 1210. If  of A's amount is equal to  of B's amount, how much amount does B have?",
+        answer: "Rs. 484",
+        options: [
+            "Rs. 460",
+            "Rs. 484",
+            "Rs. 550",
+            "Rs. 664"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Father is aged three times more than his son Ronit. After 8 years, he would be two and a half times of Ronit's age. After further 8 years, how many times would he be of Ronit's age?",
+        answer: "2 times",
+        options: [
+            "2 times",
+            "2.5 times",
+            "2.75 times",
+            "3 times",
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "The ratio between the length and the breadth of a rectangular park is 3 : 2. If a man cycling along the boundary of the park at the speed of 12 km/hr completes one round in 8 minutes, then the area of the park (in sq. m) is:",
+        answer: "153600",
+        options: [
+            "15360",
+            "153600",
+            "30720",
+            "307200",
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "An accurate clock shows 8 o'clock in the morning. Through how may degrees will the hour hand rotate when the clock shows 2 o'clock in the afternoon?",
+        answer: "180°",
+        options: [
+            "144°",
+            "150°",
+            "168°",
+            "180°",
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "The cost price of 20 articles is the same as the selling price of x articles. If the profit is 25%, then the value of x is:",
+        answer: "16",
+        options: [
+            "15",
+            "16",
+            "18",
+            "25",
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "Out of 7 consonants and 4 vowels, how many words of 3 consonants and 2 vowels can be formed?",
+        answer: "25200",
+        options: [
+            "210",
+            "1050",
+            "25200",
+            "21400"
+        ]
+    },
+];
